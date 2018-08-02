6c9af596e8bf971bf406bb3f0d031b58466ae2e5
ttacompu <ttacompu@gmail.com>
2018-08-02 07:50:17 -0400

    toggle done

--- src/js/Toggle.js
+++ src/js/Toggle.js
@@ -3,19 +3,24 @@ import ReactDOM from "react-dom";
 import {Switch} from './Switch'
 import "./app.css";
 
-
-
 class Toggle extends Component{
     state = { on : true};
-    
+    toggle = () => this.setState( ({on}) =>({on : !on}), 
+        () => this.props.onToggle(this.state.on) )
+    constructor(props){
+        super(props);
+    }
+
     render ( ){
         return (<Switch on={this.state.on} onClick={this.toggle} />);
     }
 }
 
-const Usage = ( onToggle = (...args) => console.log('onToggle', ...args) ) => (
-    <Toggle onToggle={onToggle} />
-)
+const Usage = ({onToggle = (...args) => console.log('onToggle', ...args), }) =>{
+    return (
+        <Toggle onToggle={onToggle} />
+    )
+} 
 
 ReactDOM.render(
     <div  style={{
