4789178b7bbe64f7e0ac8219d045799680f5b783
Aung <taung@cgsh.com>
2018-08-02 12:14:03 -0400

    context api

--- src/js/Toggle.js
+++ src/js/Toggle.js
@@ -3,7 +3,27 @@ import ReactDOM from "react-dom";
 import {Switch} from './Switch'
 import "./app.css";
 
+const ToggleContext = React.createContext()
+
 class Toggle extends Component{
+    static On = ({children})=>(
+        <ToggleContext.Consumer>
+            { contextValue => contextValue.on ? children : null }
+        </ToggleContext.Consumer>
+    )
+
+    static Off = ({children})=>(
+        <ToggleContext.Consumer>
+            { contextValue => contextValue.on ? null : children }
+        </ToggleContext.Consumer>
+    )
+
+    static Button = (props)=>(
+        <ToggleContext.Consumer>
+            { contextValue => (<Switch on={contextValue.on} onClick={contextValue.toggle} {...props} />) }
+        </ToggleContext.Consumer>
+    )
+    
     state = { on : true};
     toggle = () => this.setState( ({on}) =>({on : !on}), 
         () => this.props.onToggle(this.state.on) )
@@ -12,13 +32,24 @@ class Toggle extends Component{
     }
 
     render ( ){
-        return (<Switch on={this.state.on} onClick={this.toggle} />);
+        return (
+            <ToggleContext.Provider value={{on : this.state.on, toggle : this.toggle }}>
+                    {this.props.children}
+            </ToggleContext.Provider>
+        )
     }
 }
 
 const Usage = ({onToggle = (...args) => console.log('onToggle', ...args), }) =>{
     return (
-        <Toggle onToggle={onToggle} />
+        <Toggle onToggle={onToggle} >
+            <Toggle.Off>Button is off</Toggle.Off>
+            <Toggle.On>Button is on</Toggle.On>
+            <div>
+                <Toggle.Button />
+            </div>
+            
+        </Toggle >
     )
 } 
 
