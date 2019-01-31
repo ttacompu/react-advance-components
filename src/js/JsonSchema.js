ce1504927b76733bbb836d1c3596427417ad1295
ttacompu@gmail.com <taung@cgsh.com>
2019-01-31 14:21:03 -0500

    messages!

--- src/js/JsonSchema.js
+++ src/js/JsonSchema.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Form from 'react-jsonschema-form'
+import './transition.css'
+import { Motion, spring } from 'react-motion';
 
 const schema = {
     type: 'object',
@@ -8,6 +10,42 @@ const schema = {
         firstName: { type: 'string', default: 'Dan' },
         lastName: { type: 'string', default: 'Abramov' },
     },
+}
+
+const Transition = () => (
+    <Motion
+        defaultStyle={{ opacity: 0.01 }}
+        style={{ opacity: spring(1) }}
+    >
+        {interpolatingStyle => (
+            <h1 style={interpolatingStyle}>Hello React</h1>
+        )}
+    </Motion>
+
+)
+
+const style = {
+    color: 'palevioletred',
+    backgroundColor: 'papayawhip',
+}
+
+class FontSize extends React.Component {
+    handleChange({target}){
+        this.setState({ value : Number(target.value)});
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: 16,
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    render(){
+        return (
+            <input type="text" value={this.state.value} onChange={this.handleChange} style={ {fontSize : this.state.value} }></input>
+        )
+    }
 }
 
 
@@ -24,7 +62,13 @@ class JSONSchemaForm extends React.Component {
 
     render() {
         return (
-            <Form schema={schema} onSubmit={this.handleSubmit} />
+            <div>
+                <Form schema={schema} onSubmit={this.handleSubmit} />
+                <Transition></Transition>
+                <button style={style}>Post</button>
+                <FontSize />
+
+            </div>
         )
     }
 
