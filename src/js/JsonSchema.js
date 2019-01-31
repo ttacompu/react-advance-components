a6c700f757038e083fdb4bb2543187d0d848c8db
ttacompu@gmail.com <taung@cgsh.com>
2019-01-31 16:16:31 -0500

    push

--- src/js/JsonSchema.js
+++ src/js/JsonSchema.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Form from 'react-jsonschema-form'
 import './transition.css'
 import { Motion, spring } from 'react-motion';
+import radium from 'radium';
 
 const schema = {
     type: 'object',
@@ -24,14 +25,21 @@ const Transition = () => (
 
 )
 
-const style = {
-    color: 'palevioletred',
-    backgroundColor: 'papayawhip',
+const styles = {
+    backgroundColor: '#ff0000',
+    width: 320,
+    padding: 20,
+    borderRadius: 5,
+    border: 'none',
+    outline: 'none',
+    ':hover': {
+        color: '#fff',
+    }
 }
 
 class FontSize extends React.Component {
-    handleChange({target}){
-        this.setState({ value : Number(target.value)});
+    handleChange({ target }) {
+        this.setState({ value: Number(target.value) });
     }
 
     constructor(props) {
@@ -41,12 +49,17 @@ class FontSize extends React.Component {
         }
         this.handleChange = this.handleChange.bind(this)
     }
-    render(){
+    render() {
         return (
-            <input type="text" value={this.state.value} onChange={this.handleChange} style={ {fontSize : this.state.value} }></input>
+            <input type="text" value={this.state.value} onChange={this.handleChange} style={{ fontSize: this.state.value }}></input>
         )
     }
 }
+
+const Button = () =>{
+    return (<button style={styles}>Click</button>)
+}
+const CustomButton = radium(Button);
 
 
 
@@ -65,8 +78,8 @@ class JSONSchemaForm extends React.Component {
             <div>
                 <Form schema={schema} onSubmit={this.handleSubmit} />
                 <Transition></Transition>
-                <button style={style}>Post</button>
                 <FontSize />
+                <CustomButton />
 
             </div>
         )
